564d5bf10b9d87f403fec95c848652c9765925ad
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-08-05 23:23:42 +0530

    update

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const data = { name, email, phone, subject, message };
 
       try {
-        const res = await fetch("https://portfolio-backend-seven-kappa.vercel.app//", {
+        const res = await fetch("https://portfolio-backend-seven-kappa.vercel.app/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
